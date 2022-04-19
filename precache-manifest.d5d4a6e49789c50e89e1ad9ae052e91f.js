e1df3de15ccb1e730fd589537aefa1fceea7aec1
Roman-K-I <79027503+Roman-K-I@users.noreply.github.com>
2022-04-19 05:34:31 +0300

    Add files via upload

---
+++ precache-manifest.d5d4a6e49789c50e89e1ad9ae052e91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cf8fda33dbab8cc0f1f",
+    "url": "/static/css/main.22c9cde9.chunk.css"
+  },
+  {
+    "revision": "3cf8fda33dbab8cc0f1f",
+    "url": "/static/js/main.3cf8fda3.chunk.js"
+  },
+  {
+    "revision": "e0668315b1f3567522d0",
+    "url": "/static/js/1.e0668315.chunk.js"
+  },
+  {
+    "revision": "967bc51273a7e43362c5",
+    "url": "/static/js/2.967bc512.chunk.js"
+  },
+  {
+    "revision": "a265cf8fd142fad00bac",
+    "url": "/static/js/runtime~main.a265cf8f.js"
+  },
+  {
+    "revision": "a3a91cb22af45bda23581ad18c662893",
+    "url": "/index.html"
+  }
+];
